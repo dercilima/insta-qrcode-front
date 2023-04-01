a6005fe10bdb922a19d274659a5760e6ddc36043
Derci Santos <dercilima.si@gmail.com>
2023-04-01 10:23:57 -0300

    Log de erro

--- scripts/events.js
+++ scripts/events.js
@@ -14,6 +14,7 @@ function compartilharPerfilFn() {
         window.alert("Share!")
     })
     .catch((err) => {
+        document.write(JSON.stringify(err))
         window.alert("Algo deu errado!\n" + err)
     })
 }
